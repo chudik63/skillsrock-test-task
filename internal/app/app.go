4a611e95a47b4b05324a5ca742d2c369f049551b
Vitalya <vitamin11182005@gmail.com>
2025-05-29 02:54:26 +0300

    add server starting

---
+++ internal/app/app.go
@@ -0,0 +1,69 @@
+package app
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"skillsrock-test-task/internal/config"
+	"skillsrock-test-task/internal/database/postgres"
+	"skillsrock-test-task/internal/delivery/http/v1/handler"
+	"skillsrock-test-task/internal/delivery/routes"
+	"skillsrock-test-task/pkg/logger"
+	"skillsrock-test-task/pkg/migrator"
+	"syscall"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+)
+
+const shutdownTimeout = 5 * time.Second
+
+func Run() {
+	log, err := logger.New()
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := logger.SetToCtx(context.Background(), log)
+
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatal(ctx, "Failed to load configuration", zap.Error(err))
+	}
+
+	db, err := postgres.NewDatabase(cfg.Postgres)
+	if err != nil {
+		log.Fatal(ctx, "Failed to connect to the database", zap.Error(err))
+	}
+	defer db.Close()
+
+	err = migrator.Start(cfg)
+	if err != nil {
+		log.Fatal(ctx, "Failed to run migrations", zap.Error(err))
+	}
+
+	app := fiber.New()
+
+	routes.RegistrateRoutes(app, handler.NewHandler(nil, log))
+
+	go func() {
+		if err := app.Listen(":" + cfg.HTTP.Port); err != nil {
+			log.Fatal(ctx, "Failed running the server", zap.Error(err))
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+
+	<-c
+
+	ctx, shutdown := context.WithTimeout(ctx, shutdownTimeout)
+	defer shutdown()
+
+	if err = app.ShutdownWithContext(ctx); err != nil {
+		log.Error(ctx, "failed shutting down the server", zap.Error(err))
+	}
+
+	log.Info(ctx, "Server gracefully stopped")
+}
