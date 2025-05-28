70a9e9d4d8c39a6df25da5510a6e2ec6dc5a450f
Vitalya <vitamin11182005@gmail.com>
2025-05-29 02:58:15 +0300

    add all routes

--- internal/delivery/routes/routes.go
+++ internal/delivery/routes/routes.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gofiber/swagger"
 )
 
-func RegistrateRoutes(app *fiber.App, c *handler.Handler) {
+func RegistrateRoutes(app *fiber.App, h *handler.Handler) {
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "*",
 		AllowMethods: "GET,POST,PUT,DELETE",
@@ -18,7 +18,11 @@ func RegistrateRoutes(app *fiber.App, c *handler.Handler) {
 	api := app.Group("/api")
 	v1 := api.Group("/v1")
 
-	_ = v1
+	v1.Get("/tasks", h.GetTasks)
+	v1.Get("/tasks/:id", h.GetTaskByID)
+	v1.Post("/tasks", h.CreateTask)
+	v1.Put("/tasks/:id", h.UpdateTask)
+	v1.Delete("/tasks/:id", h.DeleteTask)
 
 	app.Get("/swagger/*", swagger.New(swagger.Config{
 		URL: "/docs/swagger.json",
