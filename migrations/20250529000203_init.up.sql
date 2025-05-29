a17af2724b6e0b70777c0b0bb5e61f395d1f1ac2
Vitalya <vitamin11182005@gmail.com>
2025-05-29 03:43:56 +0300

    edit migrations

--- migrations/20250529000203_init.up.sql
+++ migrations/20250529000203_init.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS tasks (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT,
-    status TEXT CHECK (status IN ('new', 'in_progress', 'done')) DEFAULT 'new'
-    created_at TIMESTAMP DEFAULT NOW()
+    status TEXT CHECK (status IN ('new', 'in_progress', 'done')) DEFAULT 'new',
+    created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW()
 );
